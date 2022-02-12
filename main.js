0820b01e2a47a803b48ecba31f2d5df7075e69cb
satyamjhadps1 <84779436+satyamjhadps1@users.noreply.github.com>
2022-02-12 16:07:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
+}
+function modelLoaded(){
+    console.log("PoseNet Initialized")
+}
+function draw(){
+    image(video, 0, 0, 300, 300);
+}
+
+function take(){
+    save("Realtime-Filter.jpeg")
+}
+
+function gotPoses(results){
+
+    if (results.length > 0){
+        console.log(results);
+        n_x = results[0].pose.nose.x;
+        n_y = results[0].pose.nose.y;
+        console.log("Nose X = "+ n_x);
+        console.log("Nose Y = "+ n_y);       
+    }
+}
